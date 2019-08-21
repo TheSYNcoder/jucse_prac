520533b9554476be16e5b7c9b78de6037e414619
TheSYNcoder <papaigd@gmail.com>
2019-08-21 09:19:53 +0530

    final

--- ju_2nd_year/file.c
+++ ju_2nd_year/file.c
@@ -5,6 +5,9 @@ struct student{
 	char name[100];
 	int scores[5];
 };
+const int MAX =10000;
+int delarr[MAX];
+int count =0;
 typedef struct student data;
 void getdata( data *s)
 {
@@ -25,6 +28,7 @@ void write()
 	FILE *fp = fopen("student.dat" , "w");
 	data s;
 	getdata(&s);
+	count++;
 	fseek( fp , 0 , 2);
 	fwrite(&s , sizeof(data) ,1, fp);
 	fclose(fp);
@@ -35,6 +39,7 @@ void append()
 	FILE *fp = fopen("student.dat" , "a");
 	data s;
 	getdata(&s);
+	count++;
 	fseek( fp , 0 , 2);
 	fwrite(&s , sizeof(data) ,1, fp);
 	fclose(fp);
@@ -48,14 +53,16 @@ void search()
 	scanf("%d" , &roll);
 	while ( fread( &s , sizeof(data) , 1, fp)!=0)
 	{
-		if ( s.roll == roll)
+		if ( s.roll == roll && !delarr[roll])
 		{
 			printf("Record found\n");
 			printf("NAME : %s\n" , s.name);
 			printf("ROLL : %d\n" , s.roll);
 			for ( int i =0; i < 5; i++)
 			printf("SCORE %d = %d\n" ,i+1, s.scores[i]);
-			return ;
+			fclose(fp);
+			return;
+			
 		}
 	}
 	printf("Record not found :(\n");
@@ -75,37 +82,44 @@ void edit()
 	while ( fread( &(s.roll) , sizeof(int) , 1, fp)!=0)
 	{
 
-		if ( s.roll == roll)
+
+		if ( s.roll == roll && !delarr[roll])
 		{
 			printf("Record found\n");
-			printf("NAME : %s\n" , s.name);
 			printf("ROLL : %d\n" , s.roll);
 			
 			found =1;
 			
 			// fseek(fp , -1 , 1);
-			fseek(fp ,-1*(sizeof(data) + sizeof(int)) , 1);
+			// fseek(fp ,-1*(sizeof(data) ) , 1);
 			printf("ENTER NEW MODIFIED DATA :\n");
+			fseek(fp , 0 , 1);
 			getchar();
 			data ss;
 			getdata(&ss);
-			fwrite(ss.name ,sizeof(char[100]) , 1, fp );
+			int c , d;
+			c =fwrite(ss.name ,sizeof(char[100]) , 1, fp );
 			for (int i =0; i < 5; i++)
-			fwrite(&(ss.scores[i]) , sizeof(int) , 1 , fp);
+			d |=fwrite(&(ss.scores[i]) , sizeof(int) , 1 , fp);
+			if (c & d)
 			printf("Written successfully\n");
+			fclose(fp);
 			return;
 		}
-		char nam[100];
-		int score[5];
-		fread(nam , sizeof(char[100]) ,1 , fp);
-		fread(score , sizeof(int[5] ) , 1, fp);
+		
+		fread(s.name, sizeof(char[100]) ,1 , fp);
+		for (int i =0; i < 5; i++)
+		fread(&(s.scores[i]) , sizeof(int ) , 1, fp);
 		offset++;
+
 	}
 	if (!found)
 	{
 		printf("Record not found :(\n");
-		return;
+		
 	}
+	fclose(fp);
+	return;
 	
 	
 
@@ -114,20 +128,79 @@ void display()
 {
 	FILE *fp= fopen("student.dat" , "r");
 	data s;
-	while ( fread(&s , sizeof(data), 1 ,fp)!=0)
+	int rec =0;
+	printf("The details are of %d students are :\n" , count);
+	while (!feof(fp))
 	{
-		getchar();
+		// getchar();
+		int c =fread(&s , sizeof(data) , 1, fp);
+		if ( delarr[s.roll])
+			continue;
 		printf("ROLL :%d\n" , s.roll);
 		printf("NAME : %s\n" , s.name);
 		
 		for ( int i =0; i < 5; i++)
 		printf("SCORE IN SUB %d = %d\n" , i+1 , s.scores[i]);
 		printf("\n"); 
-
+		rec++;
+		if ( rec == count)
+			return;
+		if ( !c)
+			break;
 	}
 
 	printf("\n");
 	fclose(fp);
+}
+
+void delet()
+{
+	printf("ENTER THE ROLL TO DELTE\n");
+	FILE *fp = fopen("student.dat" , "r+");
+	int roll;
+	scanf("%d" , &roll);
+	data s;
+	while ( fread(&s , sizeof(data) , 1, fp)!=0)
+	{
+		if ( s.roll == roll)
+		{
+			printf("Record. found\n");
+			delarr[roll]++;
+			printf("Record deleted !!\n");
+			fclose(fp);
+			return;
+		}
+	}
+	printf("Record not found :(\n");
+	fclose(fp);
+	return;
+
+}
+void phy_delete()
+{
+	printf("ENTER THE ROLL TO DELETE\n");
+	int roll;
+	scanf("%d" , &roll);
+	FILE *fp = fopen("student.dat" ,"r+");
+	FILE *ff = fopen("temp.dat" ,"w");
+	data s;
+	while ( fread(&s , sizeof(data) , 1, fp)!= 0)
+	{
+		if ( s.roll == roll)
+		{
+			continue;
+
+		}
+		fwrite(&s , sizeof(data ), 1, ff);
+	}
+	remove("student.dat");
+	printf("'OLD FILE REMOVED'\n" );
+	rename("temp.dat" , "student.dat");
+	printf("NEW FILE CREATED\n");
+	fclose(fp);
+	fclose(ff);
+
+
 }
 
 int main()
@@ -142,9 +215,11 @@ int main()
 		printf("\n");
 		printf("1. TO APPEND RECORD TO FILE\n");
 		printf("2. TO DELETE RECORD FROM FILE\n");
-		printf("3. TO SEARCH RECORD TO FILE\n");
-		printf("4. TO EDIT RECORD TO FILE\n");
-		printf("5. TO WRITE RECORD TO FILE\n");
+		printf("3. TO PHYSICALLY DELETE RECORD FROM FILE\n");
+		printf("4. TO SEARCH RECORD TO FILE\n");
+		printf("5. TO EDIT RECORD TO FILE\n");
+		printf("6. TO WRITE RECORD TO FILE\n");
+		printf("7. TO DISPLAY RECORDS OF FILE\n");
 		printf("ENTER VALUE :");
 		int n;
 		scanf("%d" , &n);
@@ -155,18 +230,21 @@ int main()
 			append();
 			break;
 			case 2:
-
+			delet();
 			break;
 			case 3:
-			search();
+			phy_delete();
 			break;
 			case 4:
-			edit();
+			search();
 			break;
 			case 5:
-			write();
+			edit();
 			break;
 			case 6:
+			write();
+			break;
+			case 7:
 			display();
 			break;
 			default:
