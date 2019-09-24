f77c63b66d89fab04949ec2e67bc020e318d8d4a
theSYNcoder <papaigd@gmail.com>
2019-09-25 01:25:42 +0530

    ghjk

--- ju_2nd_year/oop/assg3/q14.cpp
+++ ju_2nd_year/oop/assg3/q14.cpp
@@ -5,18 +5,35 @@ class Balance{
 	string acc_no;
 	int balance;
 	string  date;
-	
-	
+	friend void operate( Transaction & , Balance &b);
 
 };
+class BalanceList{
+	
+}
 
 class Transaction{
 	string acc_no;
 	int balance;
 	string date;
-	
-
+	friend void operate( Transaction &,  Balance &);
 };
+
+void operate( Transaction tr & , Balance &b){
+	cout << "Enter w for withdrawal or d for deposit :";
+	char ch ;
+	cin >> ch;
+	if ( ch == 'w' || ch == 'W')
+	{
+		//withdraw
+
+	}
+	else
+	{
+		//Deposit
+	}
+	
+}
 
 
 int main()
